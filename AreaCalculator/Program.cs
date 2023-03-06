a7eb593b97981ae049bf18af139c0bb07583542d
LilyLin395135 <lilylin395135@gmail.com>
2023-03-06 23:26:22 +0800

    加入專案檔案。

---
+++ AreaCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿//-(分析)大方向：
+//    1.使用者輸入（互動）
+//    2.邏輯（寫在Room這個類別裡面)
+//      unit : feet or meters(field)
+//      length(field)
+//      wide(field)
+//      area = length * width
+//      conversionRate = 0.09290304
+//      see if it’s feet : square meters = conversionRate * square feet
+//      it’s meters : square feet = square meters/conversionRate
+//    3.系統呈現內容（會變化的內容 長、寬、面積
+//- 邏輯的部份，如果換單位、單位所乘的數字變了  重複內容太多要改太多地方了。如何消除重複
+//- 物件導向：真實世界的物件，抽象成程式導向的類別屬性。
+//物件的關鍵字”new”寫出A物件、再new出B物件
+
+using AreaCalculator;
+
+var usersRoom = new Room(1,2,"feet");
+
+Console.WriteLine("Enter \"feet\" or \"meters\" you want to use in calculating area.");
+usersRoom.Unit = Console.ReadLine();
+if (usersRoom.MustFeetOrMeters(usersRoom.Unit) ==true)
+{
+    Console.WriteLine($"What is the length of the room in {usersRoom.Unit}?");
+    string length = Console.ReadLine();
+    Console.WriteLine($"What is the width of the room in {usersRoom.Unit}?");
+    string width = Console.ReadLine();
+
+    usersRoom.IfLengthWidthIsNumberCalculateArea(length, width);
+}
---
+++ AreaCalculator/Room.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculator
+{
+    public class Room
+    {
+        private double _length;
+        private double _width;
+        private string _unit;
+        public Room(double length, double width, string unit)
+        {
+            _length = length;
+            _width = width;
+            _unit = unit;
+        }
+        public double Length
+        {
+            get { return _length; }
+            set { _length = value; }
+        }
+        public double Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+        public string Unit
+        {
+            get { return _unit; }
+            set { _unit = value; }
+        }
+
+        string wrongMessage= "You must input numbers! You bad bad!>A<";
+        private readonly Dictionary<string, double> _unitAndNumber = new()
+        {
+            { "feet", 0.09290304 },
+            { "meters", (1/0.09290304) },
+        };
+
+        public bool MustFeetOrMeters(string Unit)
+        {
+            if (_unitAndNumber.ContainsKey(Unit))             
+            {
+                return true;
+            }           
+            Console.WriteLine(wrongMessage);
+            return false;
+        }
+        public double IfLengthWidthIsNumberCalculateArea(string length, string width)
+        {
+            double resultLength, resultWidth;
+            if (double.TryParse(length, out resultLength))
+            {                
+                if (double.TryParse(width, out resultWidth))
+                {
+                    Console.WriteLine($"You entered dimensions of {length} {Unit} by {width} {Unit}.");
+                    double area = resultLength * resultWidth;
+                    double transferArea = area * _unitAndNumber[Unit];
+                    Console.WriteLine($"The area is \n{area} square {Unit} \n{transferArea} square meters");//最後這個單位不知道怎麼樣才能叫出另外一個
+                }
+                else
+                {
+                    Console.WriteLine(wrongMessage);
+                }
+            }
+            else
+            {
+                Console.WriteLine(wrongMessage);
+            }
+            return 0;
+        }
+
+    }
+    
+}
